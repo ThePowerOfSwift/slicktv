b8b33d71f6a7bfc06013e1ea4c87b2e22853d399
stanchiang <stanchiang23@gmail.com>
2015-10-28 20:52:41 -0700

    added promisekit

--- Podfile
+++ Podfile
@@ -4,3 +4,4 @@ use_frameworks!
 
 pod 'Alamofire', '~> 1.3.1'
 pod 'SwiftyJSON', '~> 2.2.0'
+pod 'PromiseKit', '~> 2.0'
--- slicktv/network.swift
+++ slicktv/network.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import PromiseKit
 
 class Network {
     static let sharedInstance = Network()
